3c9175972ddb08eedd2516a3e5c157a3dc1c35ca
Yoavmegadish <yoavo74@gmail.com>
2024-10-30 23:15:24 +0200

    initial commit

--- app/src/main/java/com/example/final_project/MainActivity.java
+++ app/src/main/java/com/example/final_project/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.final_project;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    TextView tv;
+    Button logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+         mAuth = FirebaseAuth.getInstance();
+        tv = findViewById(R.id.textView);
+        logout = findViewById(R.id.logoutbtn);
+
+    };
+
+
     }
-}
+
